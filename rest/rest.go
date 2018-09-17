e8a8d8b8d128296957950cc57586412bba5368d8
Kusubooru <kusubooru@gmail.com>
2018-09-17 22:32:01 +0300

    Allow CORS

--- rest/rest.go
+++ rest/rest.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/NYTimes/gziphandler"
 	"github.com/kusubooru/monban/monban"
@@ -38,10 +39,33 @@ type server struct {
 // NewServer initializes and returns a new HTTP server.
 func NewServer(auth monban.AuthService) http.Handler {
 	s := &server{mux: http.NewServeMux(), auth: auth}
-	s.handlers = gziphandler.GzipHandler(s.mux)
+	s.handlers = gziphandler.GzipHandler(allowCORS(s.mux))
 	s.mux.Handle("/api/v0/auth/login", handler(s.handleLogin))
 	s.mux.Handle("/api/v0/auth/refresh", handler(s.handleRefresh))
 	return s
+}
+
+// allowCORS allows Cross Origin Resource Sharing from any origin.
+// Don't do this without consideration in production systems.
+func allowCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if origin := r.Header.Get("Origin"); origin != "" {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			if r.Method == "OPTIONS" && r.Header.Get("Access-Control-Request-Method") != "" {
+				preflightHandler(w, r)
+				return
+			}
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	headers := []string{"Content-Type", "Accept"}
+	w.Header().Set("Access-Control-Allow-Headers", strings.Join(headers, ","))
+	methods := []string{"GET", "HEAD", "POST", "PUT", "DELETE"}
+	w.Header().Set("Access-Control-Allow-Methods", strings.Join(methods, ","))
+	return
 }
 
 // ServeHTTP satisfies the http.Handler interface for a server.
